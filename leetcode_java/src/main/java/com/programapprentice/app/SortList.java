4484c0cfc03e80b810028077ab815d1af8da9dff
rjiang <rjiang@quantcast.com>
2014-07-31 15:17:17 -0700

    add empty sort list

---
+++ leetcode_java/src/main/java/com/programapprentice/app/SortList.java
@@ -0,0 +1,24 @@
+package com.programapprentice.app;
+
+/**
+ * @author rjiang
+ * @date 7/31/14
+ */
+
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ * */
+public class SortList {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+
+    }
+}
