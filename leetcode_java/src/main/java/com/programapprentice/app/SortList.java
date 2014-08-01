68d55723a6ea1adbe37abd894ee7663e0a7dc493
rjiang <rjiang@quantcast.com>
2014-08-01 08:44:16 -0700

    add accepted Sort List

--- leetcode_java/src/main/java/com/programapprentice/app/SortList.java
+++ leetcode_java/src/main/java/com/programapprentice/app/SortList.java
@@ -9,7 +9,7 @@ package com.programapprentice.app;
  * Sort a linked list in O(n log n) time using constant space complexity.
  * */
 public class SortList {
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) {
@@ -19,6 +19,54 @@ public class SortList {
     }
 
     public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode preSlow = head;
+        ListNode fast = head;
 
+        while(slow != null && fast != null) {
+            if(slow != head) {
+                preSlow = preSlow.next;
+            }
+            slow = slow.next;
+            fast = fast.next;
+            fast = fast == null ? null : fast.next;
+        }
+        preSlow.next = null;
+        fast = slow;
+        slow = head;
+        fast = sortList(fast);
+        slow = sortList(slow);
+        return mergeList(slow, fast);
+    }
+
+    public ListNode mergeList(ListNode one, ListNode two) {
+        ListNode head = new ListNode(0);
+        ListNode a = one;
+        ListNode b = two;
+        ListNode p = head;
+
+        while(a != null && b != null) {
+            if(a.val < b.val) {
+                p.next = a;
+                a = a.next;
+            } else if( a.val > b.val) {
+                p.next = b;
+                b = b.next;
+            } else {
+                p.next = a;
+                a = a.next;
+                p = p.next;
+                p.next = b;
+                b = b.next;
+            }
+            p = p.next;
+            p.next = null;
+        }
+
+        p.next = a == null ? b : a;
+        return head.next;
     }
 }
--- leetcode_java/src/test/java/com/programapprentice/app/Test_SortList.java
+++ leetcode_java/src/test/java/com/programapprentice/app/Test_SortList.java
@@ -1,8 +1,31 @@
 package com.programapprentice.app;
 
+import org.junit.Test;
+
 /**
  * @author rjiang
  * @date 7/31/14
  */
 public class Test_SortList {
+    SortList obj = new SortList();
+
+    public void print(SortList.ListNode head) {
+        SortList.ListNode p = head;
+        while(p != null) {
+            System.out.print(p.val + "\t");
+            p = p.next;
+        }
+        System.out.println("");
+    }
+
+    @Test
+    public void test1() {
+        SortList.ListNode head = new SortList.ListNode(87);
+        head.next = new SortList.ListNode(234);
+        head.next.next = new SortList.ListNode(462);
+        head.next.next.next = new SortList.ListNode(62);
+        head = obj.sortList(head);
+        print(head);
+    }
+
 }
