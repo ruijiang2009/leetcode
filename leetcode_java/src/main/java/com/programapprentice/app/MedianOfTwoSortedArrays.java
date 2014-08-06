eeca7d02044fd4e4cfc3ba15524184fdd4e9cecd
rjiang <rjiang@quantcast.com>
2014-08-06 12:32:58 -0700

    unfinished Median of Two Sorted Arrays

---
+++ leetcode_java/src/main/java/com/programapprentice/app/MedianOfTwoSortedArrays.java
@@ -0,0 +1,27 @@
+package com.programapprentice.app;
+
+/**
+ * @author rjiang
+ * @date 8/6/14
+ */
+public class MedianOfTwoSortedArrays {
+
+    /**
+     * There are two sorted arrays A and B of size m and n respectively.
+     * Find the median of the two sorted arrays.
+     * The overall run time complexity should be O(log (m+n)).
+     * */
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int ahigh = A.length;
+        int alow = 0;
+        int amid = 0;
+
+        int bhigh = B.length;
+        int blow = 0;
+        int bmid = 0;
+
+        int target = 0;
+        if ((A.length + B.length) & 1) == 1
+        while(amid)
+    }
+}
