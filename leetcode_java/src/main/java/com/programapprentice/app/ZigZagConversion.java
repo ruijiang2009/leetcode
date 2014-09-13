01958f662f87040ecdb1261fb363a71ff9ed398a
Rui Jiang <rui.jiang.dev@gmail.com>
2014-09-12 17:15:07 -0700

    add uncompleted zigzag conversion

---
+++ leetcode_java/src/main/java/com/programapprentice/app/ZigZagConversion.java
@@ -0,0 +1,16 @@
+package com.programapprentice.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ruijiang
+ * Date: 9/9/14
+ * Time: 7:52 PM
+ */
+public class ZigZagConversion {
+    public String convert(String s, int nRows) {
+        StringBuilder sb = new StringBuilder();
+        for(int row = 1; row <= nRows; row++) {
+            
+        }
+    }
+}
---
+++ leetcode_java/src/test/java/com/programapprentice/app/Test_ZigZagConversion.java
@@ -0,0 +1,50 @@
+package com.programapprentice.app;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ruijiang
+ * Date: 9/11/14
+ * Time: 5:51 PM
+ */
+public class Test_ZigZagConversion {
+    ZigZagConversion obj = new ZigZagConversion();
+    @Test
+    public void test1() {
+        String input = "PAYPALISHIRING";
+        int nRows = 3;
+        String expected = "PAHNAPLSIIGYIR";
+        String actual = obj.convert(input, nRows);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        String input = "ABC";
+        int nRows = 2;
+        String expected = "ACB";
+        String actual = obj.convert(input, nRows);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        String input = "ABCDEFGHIJKLMN";
+        int nRows = 4;
+        String expected = "ACB";
+        String actual = obj.convert(input, nRows);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test4() {
+        String input = "ABCD";
+        int nRows = 2;
+        String expected = "ACBD";
+        String actual = obj.convert(input, nRows);
+        assertEquals(expected, actual);
+    }
+}
