d544137efffa448f1d6a367e34e68bea6510188b
Rui Jiang <rui.jiang.dev@gmail.com>
2014-11-02 01:33:31 -0700

    add unfinished Find Minimum in Rotated Sorted Array II

---
+++ leetcode_java/src/main/java/com/programapprentice/app/FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,93 @@
+package com.programapprentice.app;
+
+/**
+ * User: ruijiang
+ * Date: 11/2/14
+ * Time: 12:08 AM
+ */
+/**
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ *
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ *
+ * Find the minimum element.
+ * The array may contain duplicates.
+ * */
+// reference http://blog.csdn.net/linhuanmars/article/details/40449299
+public class FindMinimuminRotatedSortedArrayII {
+    public int findMinV2(int[] num) {
+        if(num == null) {
+            return 0;
+        }
+        int low = 0;
+        int high = num.length - 1;
+        int mid = 0;
+        int midlow = 0;
+        int midhigh = 0;
+        while(low <= high) {
+            mid = (low + high) / 2;
+            midlow = (mid - 1 + num.length) % num.length;
+            midhigh = (mid + 1) % num.length;
+            if(num[midlow] > num[mid] && num[midhigh] >= num[mid]) {
+                return num[mid];
+            }
+            if(num[mid] > num[low]) {
+                low = midhigh;
+            } else if(num[mid] < num[low]){
+                high = midlow;
+            } else {
+                // num[mid] == num[low]
+                low = midhigh;
+            }
+//            if(num[midlow] > num[mid] && num[midhigh] >= num[mid]) {
+//                return num[mid];
+//            }
+//            if()
+//            if(num[mid] > num[high]) {
+//                low = midhigh;
+//            } else {
+//                high = midlow;
+//            }
+        }
+        return num[mid];
+    }
+
+    public int findMin(int[] num) {
+        if(num == null || num.length == 0) {
+            return 0;
+        }
+        if(num.length ==1) {
+            return num[0];
+        }
+        if(num[0] < num[num.length - 1]) {
+            return num[0];
+        }
+        int low = 0;
+        int high = num.length - 1;
+        int mid = 0;
+        int midlow = 0;
+        int midhigh = 0;
+        while(low <= high) {
+            mid = (low + high) / 2;
+            midlow = (mid - 1 + num.length) % num.length;
+            midhigh = (mid + 1) % num.length;
+
+            if(num[midlow] > num[mid] && num[midhigh] >= num[mid]) {
+                break;
+            }
+            if(num[mid] > num[low]) {
+//                low = mid + 1;
+//                low = midhigh;
+                low = mid;
+            } else if(num[mid] < num[low]){
+//                high = mid - 1;
+//                high = midlow;
+                high = mid;
+            } else {
+                // num[mid] == num[low]
+                low++;
+            }
+        }
+        return num[mid];
+    }
+}
---
+++ leetcode_java/src/test/java/com/programapprentice/app/Test_FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,46 @@
+package com.programapprentice.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: ruijiang
+ * Date: 11/2/14
+ * Time: 12:14 AM
+ */
+public class Test_FindMinimuminRotatedSortedArrayII {
+    FindMinimuminRotatedSortedArrayII obj = new FindMinimuminRotatedSortedArrayII();
+
+    @Test
+    public void test1() {
+        int[] input = {4, 5, 6, 7, 0, 1, 2};
+        int expected = 0;
+        int actual = obj.findMin(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        int[] input = {4, 5, 6, 7, 0, 0, 1, 1, 2};
+        int expected = 0;
+        int actual = obj.findMin(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        int[] input = {1, 1};
+        int expected = 1;
+        int actual = obj.findMin(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test4() {
+        int[] input = {3, 3, 1, 3};
+        int expected = 1;
+        int actual = obj.findMin(input);
+        assertEquals(expected, actual);
+    }
+}
