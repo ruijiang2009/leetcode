a819d61cf9ea39f06fa0a31bfda5fcce8af0ef62
Rui Jiang <rui.jiang.dev@gmail.com>
2014-11-07 22:33:50 -0800

    update unit test for cpp Reverse Integer

--- leetcode_cpp/test/reverse_integer_test.cc
+++ leetcode_cpp/test/reverse_integer_test.cc
@@ -6,6 +6,10 @@
  */
 #include "reverse_integer.h"
 #include "gtest/gtest.h"
+#include <iostream>
+#include <bitset>
+
+using namespace::std;
 
 TEST(ReverseInteger, One) {
     Solution obj;
@@ -19,6 +23,36 @@ TEST(ReverseInteger, Two) {
     Solution obj;
     int input = -123;
     int expected = -321;
+    int actual = obj.reverse(input);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(ReverseInteger, Three) {
+    Solution obj;
+    int input = -443946297;
+    int expected = -792649344;
+    int actual = obj.reverse(input);
+    cout<< "input    " << bitset<32>(input) << " decimal: " << input << endl;
+    cout<< "expected " << bitset<32>(expected) << " decimal: " << expected << endl;
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(ReverseInteger, Four) {
+    Solution obj;
+    int input = 2147483643;
+    int expected = -831119884;
+    cout<< "input    " << bitset<32>(input) << endl;
+    cout<< "expected " << bitset<32>(expected) << " decimal: " << expected << endl;
+    int actual = obj.reverse(input);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(ReverseInteger, Five) {
+    Solution obj;
+    int input = 2147483644;
+    int expected = 168880116;
+    cout<< "input    " << bitset<32>(input) << " decimal: " << input << endl;
+    cout<< "expected " << bitset<32>(expected) << " decimal: " << expected << endl;
     int actual = obj.reverse(input);
     EXPECT_EQ(expected, actual);
 }
